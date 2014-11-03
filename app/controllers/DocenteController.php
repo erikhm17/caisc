a0b1892a2679dc9fb9d6a937664030d0059c9d4f
Juan E <juan@escodigo.com>
2014-11-03 07:06:10 -0500

    Metodos agregados, para el crud de docente

--- app/controllers/DocenteController.php
+++ app/controllers/DocenteController.php
@@ -7,25 +7,101 @@ class DocenteController extends BaseController
 		$docentes = Docente::all();
 		return View::make('docente.index',array('docentes'=>$docentes));
 	}
-	public function nuevo()
+	public function profile($cod = null)
 	{
-		return View::make('docente.nuevo');
+		if (is_null($cod) or ! is_numeric($cod))
+		{
+			return Redirect::to('404.html');
+		} else {
+			$docente = Docente::where('codDocente','=',$cod)->firstOrFail();
+			if (is_object($docente))
+			{
+				return View::make('docente.profile',array('docente'=>$docente));
+			} else {
+				return Redirect::to('404.html');
+			}
+		}
 	}
-	public function crear()
+	public function add()
+	{
+		return View::make('docente.add');
+	}
+	public function insert()
 	{
 		$respuesta = Docente::agregar(Input::all());
 		if($respuesta['error']==true)
 		{
-			return Redirect::to('docente/nuevo')->with('mensaje',$respuesta['mensaje'])->withInput();
-		} else 
-		{
-			return Redirect::to('docente')->with('mensaje',$respuesta['mensaje']);
-
+			return Redirect::to('docente/add')->with('mensaje',$respuesta['mensaje'])->withInput();
+		} else {
+			return Redirect::to('docentes')->with('mensaje',$respuesta['mensaje']);
 		}
 	}
-	public function actualizar()
+	public function edit($cod=null)
 	{
-		return View::make('blank');
+		if(is_null($cod))
+		{
+			return Redirect::to('404.html');
+		} else {
+			$docente = Docente::where('codDocente','=',$cod)->firstOrFail();
+			return View::make('docente.edit',array('docente'=>$docente));
+		}
 	}
+	public function update()
+	{
+		$input=Input::get('codDocente');
+		if(is_null($cod))
+		{
+			Redirect::to('404.html');
+		} else {
+			$docente = Docente::where('codDocente','=',$cod)->firstOrFail();
+			if(is_object($docente))
+			{
+				$docente->nombre = Input::get('nombre');
+				$docente->apellidos = Input::get('apellidos');
+				$docente->email = Input::get('email');
+				$docente->telefono = Input::get('telefono');
+				$docente->save();
+				return Redirect::to('docentes');
+			} else {
+				Redirect::to('500.html');
+			}
+		}
+	}
+	public function delete($cod = null)
+	{
+		if(is_null($cod))
+		{
+			Redirect::to('404.html');
+		} else {
+			$docente = Docente::where('codDocente','=',$cod)->firstOrFail();
+			if(is_object($docente))
+			{
+				$docente->estado = '1';
+				$docente->save();
+				return Redirect::to('docentes');
+			}
+		}
+	}
+	public function login()
+	{
+		$email = null;
+		if(is_null($email))
+		{
+			return View::make('docente/login');
+		} else {
+			$docente = Docente::where('email','=',$email)->first();
+			// Activar sessión
+			return Redirect::to('docente/profile/'.$docente->codDocente);
+		}
+	}
+	public function loginInit()
+	{
 
+	}
+	public function logout()
+	{
+		// Destruir sessión
+		// Redirigir a Login
+		return Redirect::to('docente/login.html');
+	}
 }
