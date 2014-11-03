ca2c8f1ccf4d967e796af5004162a37892488729
Juan E <juan@escodigo.com>
2014-11-03 07:01:09 -0500

    rutas para el modulo Docente

--- app/routes.php
+++ app/routes.php
@@ -20,10 +20,16 @@ Route::get('404.html',array('uses'=>'ErrorController@mostrar404'));
 Route::get('500.html',array('uses'=>'ErrorController@mostrar500'));
 Route::get('blank.html',array('uses'=>'ErrorController@blank'));
 // Docente
-Route::get('docente',array('uses'=>'DocenteController@index'));
-Route::get('docente/nuevo.html',array('uses'=>'DocenteController@nuevo'));
-Route::get('docente/actualizar.html',array('uses'=>'DocenteController@actualizar'));
-Route::post('docente/crear',array('uses'=>'DocenteController@crear'));
+Route::get('docentes',array('uses'=>'DocenteController@index'));
+Route::get('docente/add.html',array('uses'=>'DocenteController@add'));
+Route::get('docente/login.html',array('uses'=>'DocenteController@login'));
+Route::post('docente/login',array('uses'=>'DocenteController@loginInit'));
+Route::get('docente/logout.html',array('uses'=>'DocenteController@logout'));
+Route::get('docente/edit/{cod}',array('uses'=>'DocenteController@edit'))->where('cod','[0-9]+');
+Route::post('docente/update.html',array('uses'=>'DocenteController@update'));
+Route::post('docente/insert.html',array('uses'=>'DocenteController@insert'));
+Route::get('docente/profile/{cod}',array('uses'=>'DocenteController@profile'))->where('cod','[0-9]+');
+Route::get('docente/delete/{cod}',array('uses'=>'DocenteController@delete'))->where('cod','[0-9]+');
 
 //Modulos mantenimiento
 Route::get('modulo',array('uses'=>'ModuloController@index'));
