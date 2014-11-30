54da3b15bff843ff39a2b39c50af52301e18ca1b
AyLGroup <lesly7231@gmail.com>
2014-11-29 21:19:42 -0500

    Cursos de carrera libre
    rutas

--- app/views/inicio.blade.php
+++ app/views/inicio.blade.php
@@ -170,6 +170,55 @@
                             </div>
                         </div><!-- ./col -->
                         
+                        
+
+                         <div class="col-lg-4 col-xs-6">
+                            <!-- small box -->
+                            <div class="small-box bg-red">
+                                <div class="inner">
+                                    <h3>
+                                        <div>
+                                            Cursos
+                                        </div>
+                                        <div> Carrera Libre</div>
+                                    </h3>
+                                    <p>
+                                        Login
+                                    </p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-stats-bars"></i>
+                                </div>
+                                <a href="CursosLibres/create.html" class="small-box-footer">
+                                    Click <i class="fa fa-arrow-circle-right"></i>
+                                </a>
+                            </div>
+                             </div>
+
+                         <div class="col-lg-4 col-xs-6">
+                            <!-- small box -->
+                            <div class="small-box bg-purple">
+                                <div class="inner">
+                                    <h3>
+                                        <div>
+                                            Cursos
+                                        </div>
+                                        <div>Carrera Tecnica</div>
+                                    </h3>
+                                    <p>
+                                        Login
+                                    </p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-stats-bars"></i>
+                                </div>
+                                <a href="CursosTecnica/create.html" class="small-box-footer">
+                                    Click <i class="fa fa-arrow-circle-right"></i>
+                                </a>
+                            </div>
+
+                        </div><!-- ./col -->
+
                     </div><!-- /.row -->
 
                     
