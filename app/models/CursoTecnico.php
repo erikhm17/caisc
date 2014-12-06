b19fe947b0ab8dd34886b433ef2d3b6bb53695b4
AyLGroup <lesly7231@gmail.com>
2014-12-06 10:52:22 -0500

    modelo para cursos tecnicos validado

--- app/models/CursoTecnico.php
+++ app/models/CursoTecnico.php
@@ -1,47 +1,57 @@
 <?php
 
-class CursoTecnico extends Eloquent {
+class CursoLibre extends Eloquent {
 
-	protected $table = 'curso_ct';
-	protected $fillable = array('codCurso_ct','nombre','modulo','estado','codCarrera');
+	protected $table = 'curso_cl';
+	protected $fillable = array('id','nombre','horas_academicas','estado','updated_at','created_up');
 	
 	public static function agregar($input)
 	{
 		$respuesta = array();
 		$reglas = array(
-			'nombre'=>array('required','max:30'),
-			'modulo'=>array('required','max:2'),
-			'estado'=>array('required','max:1'),
-			'codCarrera'=>array('required','max:10'),
-		);
+				'id'=>array('required','max:10'),
+				'nombre'=>array('required','max:30'),
+				'horas_academicas'=>array('required','max:30')
+			);
+
 		$validador = Validator::make($input,$reglas);
 		if($validador->fails())
 		{
-			$respuesta['mensaje'] = $validador;
+			$respuesta['mensaje'] = 'DATOS INGRESADOS INCORRECTAMENTE';
 			$respuesta['error'] = true;
-		} else
+		} 
+		else
 		{
-			$curso = new CursoTecnico;
-			$curso->codCurso_cl = Input::get('codCurso_ct');
+			$curso = new CursoLibre;
+			$curso->id = Input::get('id');
 			$curso->nombre = Input::get('nombre');
 			$curso->horas_academicas = Input::get('horas_academicas');
 			$curso->estado = 1;
 			$curso->created_at= time();
-			$curso->updated_at = false;
-			//$curso->save();
-			if ($curso->save()) 
+			$curso->updated_at = time();
+			if(CursoLibre::find(input::get('id')))
 			{
-				Session::flash('message','Guardado correctamente!');
-				Session::flash('class','success');
+				$respuesta['mensaje'] = 'YA EXISTE ESE CODIGO';
+				$respuesta['error'] = true;
+				$respuesta['data'] = $curso;
 			}
-			else 
+			else
 			{
-				Session::flash('message','Ha ocurrido un error!');
-				Session::flash('class','danger');
-			}
-			$respuesta['mensaje'] = 'Curso Tecnico Creado';
-			$respuesta['error'] = false;
-			$respuesta['data'] = $curso_ct;
+				if ($curso->save()) 
+				{
+					$respuesta['mensaje'] = 'Curso Creado';
+					$respuesta['error'] = false;
+					$respuesta['data'] = $curso;
+				
+				}
+				else 
+				{
+					$respuesta['mensaje'] = 'DATOS INCORRECTOS';
+					$respuesta['error'] = true;
+					$respuesta['data'] = $curso;
+				
+				}	
+			}		
 		}
 		return $respuesta;
 	}
