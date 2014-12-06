cc731b31d96cf938899d8bac2c3b40728908f2ba
Antony Manuel <n.antony.nina@gmail.com>
2014-12-06 11:57:41 -0500

    Correccion

--- procedimientosingresodenotas.sql
+++ procedimientosingresodenotas.sql
@@ -1,15 +1,19 @@
+
+DELIMITER $$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `AlumnosXCurso`(IN `codcargaAcademica` VARCHAR(20))
 BEGIN
-    select N.id as idNota, A.codAlumno as idAlumno, CONCAT(A.apellidos,  ' ', A.nombre) as NombreCpt, N.notaa as Nota1, N.notab as Nota2, N.notac as Nota3
+    select N.id as idNota, A.codAlumno as "idAlumno", CONCAT(A.apellidos,  ' ', A.nombre) as "NombreCpt", N.notaa as "Nota1", N.notab as "Nota2", N.notac as "Nota3"
 	from (alumno A inner join matricula_ct M on A.codAlumno = M.codAlumno) inner join nota_ct N on N.codMatricula_ct = M.id
     where M.codCargaAcademica_ct = codcargaAcademica
     ORDER BY A.apellidos;
 END
 
+DELIMITER $$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `CursosXDocente`(IN `id_docente` INT)
 BEGIN
-    select cg.codCargaAcademica_ct as id, cu.nombre as nombre
+    select cg.codCargaAcademica_ct as "id", cu.nombre as "nombre"
 
 	from carga_academica_ct cg inner join curso_ct cu on cg.codCurso_ct = cu.codCurso_ct 
     where cg.docente_id = id_docente;
