f7bcc5320d8468ec7689d3a3407c5d964188ade5
yuresz <yuresz_14@outlook.com>
2014-12-07 10:13:10 -0500

    view matriculaCT

--- app/views/matriculaCT/index.blade.php
+++ app/views/matriculaCT/index.blade.php
@@ -11,37 +11,37 @@ Lista de Matriculas
         <h3 class="box-title"> </h3>
     </div>
     <div class="box-body table-responsive">
-    	<div id="example1_wrapper" class="dataTables_wrapper form-inline" role="grid">
-        	<div class="row">
-        		<div class="col-xs-6">
-        			<div class="dataTables_length" id="example1_length">
-        				<label><select aria-controls="example1" size="1" name="example1_length">
-        					<option selected="selected" value="10">10</option>
-        					<option value="25">25</option>
-        					<option value="50">50</option>
-        					<option value="100">100</option>
-        				</select> records per page</label>
-        			</div>
-        		</div>
-        		<div class="col-xs-6">
-        			<div id="example1_filter" class="dataTables_filter">
+        <div id="example1_wrapper" class="dataTables_wrapper form-inline" role="grid">
+            <div class="row">
+                <div class="col-xs-6">
+                    <div class="dataTables_length" id="example1_length">
+                        <label><select aria-controls="example1" size="1" name="example1_length">
+                            <option selected="selected" value="10">10</option>
+                            <option value="25">25</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                        </select> records per page</label>
+                    </div>
+                </div>
+                <div class="col-xs-6">
+                    <div id="example1_filter" class="dataTables_filter">
                         {{ HTML::link('matriculas/add.html','Agregar Matricula') }}
-        			</div>
-        		</div>
-        	</div>
+                    </div>
+                </div>
+            </div>
             <table aria-describedby="example1_info" id="example1" class="table table-bordered table-striped dataTable">
                 <thead>
                     <tr role="row">
                         <th colspan="1" rowspan="1">Codigo Matricula CT</th>
-                    	<th colspan="1" rowspan="1">Codigo Alumno</th>
+                        <th colspan="1" rowspan="1">Codigo Alumno</th>
                         <th colspan="1" rowspan="1">Codigo Carga Academica</th>
                         <th colspan="1" rowspan="1">Modulo</th>
                         <th colspan="1" rowspan="1">Action</th>
                     </tr>
                 </thead>
                 <tbody aria-relevant="all" aria-live="polite" role="alert">
-                	@foreach( $matriculas as $matricula)
-                	<tr class="odd">
+                    @foreach( $matriculas as $matricula)
+                    <tr class="odd">
                             <td class=" "><b>{{ $matricula->id }}</b></td>
                             <td class=" "><b>{{ $matricula->codAlumno }}</b></td>
                             <td class=" ">{{ $matricula->codCargaAcademica_ct }}</td>
