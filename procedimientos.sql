fcd724f293b6483d827b3c64cd431f8541b3e75d
DeHaF <berserkerfh@gmail.com>
2014-12-09 16:18:20 -0500

    Procedimeinto optimizados

--- procedimientos.sql
+++ procedimientos.sql
@@ -50,7 +50,9 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `insertarCargaAcademica_cl`(codCurso
 docente_idI int,turnoI varchar(10),grupoI int,semestreI varchar(10),fecha_inicioI date,fecha_finI date,estadoI bit,minimoI int,codAulaI varchar(10),horarioI varchar(10),diaI varchar(10))
 BEGIN
     Declare codCargaAcademica_clI varchar(10);
-    select Date_format(now(),'%m%d%H%i%s') into codCargaAcademica_clI;
+    Declare aleartorio int;
+    select 10+(RAND() * 1000) into aleartorio;
+    select Date_format(now(),'%m%d%H%i%s')+aleartorio into codCargaAcademica_clI;
     INSERT INTO `carga_academica_cl`(`codCargaAcademica_cl`, `codCurso_cl`, `docente_id`, `turno`, `grupo`, `semestre`, `fecha_inicio`, `fecha_fin`, `estado`, `minimo`) VALUES (codCargaAcademica_clI,codCurso_clI,docente_idI,turnoI,grupoI,semestreI,fecha_inicioI,fecha_finI,estadoI,minimoI);
     INSERT INTO `horario_aula`(`codAula`, `horario`, `dia`, `codCargaAcademica_ct`, `codCargaAcademica_cl`) VALUES (codAulaI ,horarioI,diaI,null,codCargaAcademica_clI);
 END
@@ -64,7 +66,9 @@ DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insertarCargaAcademica_ct`(codCurso_ctI varchar(10),docente_idI int,semestreI varchar(10),turnoI varchar(10),grupoI int,codAulaI varchar(10),horarioI varchar(10),diaI varchar(10))
 BEGIN
     Declare codCargaAcademica_ctI varchar(10);
-    select Date_format(now(),'%m%d%H%i%s') into codCargaAcademica_ctI;
+    Declare aleartorio int;
+    select 10+(RAND() * 1000) into aleartorio;
+    select Date_format(now(),'%m%d%H%i%s')+aleartorio into codCargaAcademica_ctI;
     INSERT INTO `carga_academica_ct`(`codCargaAcademica_ct`, `codCurso_ct`, `docente_id`, `semestre`, `turno`, `grupo`) VALUES (codCargaAcademica_ctI,codCurso_ctI,docente_idI,semestreI,turnoI,grupoI);
     INSERT INTO `horario_aula`(`codAula`, `horario`, `dia`, `codCargaAcademica_ct`, `codCargaAcademica_cl`) VALUES (codAulaI ,horarioI,diaI,codCargaAcademica_ctI,null);
 END
