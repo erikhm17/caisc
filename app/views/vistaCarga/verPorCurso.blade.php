249b667f0147e385f6471c88ae10a0582407bb21
DeHaF <berserkerfh@gmail.com>
2014-12-17 10:24:53 -0500

    vistas agregadas

---
+++ app/views/vistaCarga/verPorCurso.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.base_admin')
+@section('title')
+Agregar Personal <small> NUEVO PERSONAL </small>
+@stop
+@section('breadcrumb')
+<li>Agregar</li>
+@stop
+@section('content')
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>principal</title>
+	<!-- este codigo blade llama al style.css comenzando de la carpeta public como raiz-->
+	{{HTML::style('css/styleCargaIndex.css')}}
+</head>
+<body>
+	<header id="inicio">
+		<h2>Eliminar Carga</h2>
+	</header>
+		ver por curso
+</body>
+</html>
+@stop
