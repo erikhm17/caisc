cf043bf0d9d1cbd7cfdec6abbe6d048215ce785e
oscarGH <oscarpremiun@hotmail.com>
2014-12-16 21:58:38 -0500

    Modificacion triguers
    cambie solo el tipo usuario de los triguer , que empieze con mayuscula

--- trigger_usuario.sql
+++ trigger_usuario.sql
@@ -2,12 +2,12 @@ CREATE TRIGGER agregar_usuario_docente
 AFTER INSERT ON docente
 FOR EACH ROW
 INSERT INTO users(email,password,tipoUsuario,nroId,estado,created_at) 
-values (NEW.email,NEW.password,'docente',NEW.id,NEW.estado,NOW());
+values (NEW.email,NEW.password,'Docente',NEW.id,NEW.estado,NOW());
 
 CREATE TRIGGER agregar_usuario_personal 
 AFTER INSERT ON personal
 FOR EACH ROW
 INSERT INTO users(email,password,tipoUsuario,nroId,estado,created_at) 
-values (NEW.email,NEW.password,'personal',NEW.id,NEW.estado,NOW());
+values (NEW.email,NEW.password,'Personal',NEW.id,NEW.estado,NOW());
 
 
