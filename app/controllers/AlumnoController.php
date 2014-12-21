dba7b5f357a1bc007ecb86a054859dd3b09ee056
yuresz <yuresz_14@outlook.com>
2014-12-21 11:14:33 -0500

    controlador para alumno

--- app/controllers/AlumnoController.php
+++ app/controllers/AlumnoController.php
@@ -2,11 +2,35 @@
 
 class AlumnoController extends BaseController
 {
-	public function index($registros=5)
+	public function index($registros=20)
 	{
-		$datos = Alumno::paginate($registros);
-		$alumnos = Alumno::all();
-		return View::make('alumno.index',compact("datos"),array('alumnos'=>$alumnos));
+		//$cod="1";
+		//$datos = Alumno::paginate($registros);
+		$carreras = Carrera::all();
+		$alumnos=Alumno::all();
+		//$alumnos = Alumno::where('codCarrera','=',$cod)->get();
+		$datos = DB::table('alumno')
+        				->leftJoin('carrera', 'alumno.codCarrera', '=', 'carrera.id')
+        				->select('alumno.id', 'alumno.apellidos', 'alumno.nombre', 'carrera.nombre as carr', 'alumno.dni', 'alumno.estado')
+        				->get();
+		return View::make('alumno.index',compact("datos","carreras"),array('alumnos'=>$alumnos));
+	}
+
+	public function indexcarrera()
+	{
+		//$cod="1";
+		//$datos = Alumno::paginate($registros);
+		$codigo = Input::get('codCarrera');
+		$cod = (int)($codigo);
+		$carreras = Carrera::all();
+		$alumnos=Alumno::all();
+		//$alumnos = Alumno::where('codCarrera','=',$cod)->get();
+		$datos = DB::table('alumno')
+						->where('alumno.codCarrera', '=', $cod)
+        				->leftJoin('carrera', 'alumno.codCarrera', '=', 'carrera.id')
+        				->select('alumno.id', 'alumno.apellidos', 'alumno.nombre', 'carrera.nombre as carr', 'alumno.dni', 'alumno.estado')
+        				->get();
+		return View::make('alumno.listacarrera',compact("datos","carreras"),array('alumnos'=>$alumnos));
 	}
 
 	public function add()
@@ -19,6 +43,7 @@ class AlumnoController extends BaseController
 	public function insert()
 	{
 		$respuesta = Alumno::agregar(Input::all());
+		
 		if($respuesta['error']==true)
 		{
 			return Redirect::to('alumno/add.html')->withErrors($respuesta['mensaje'] )->withInput();
@@ -26,40 +51,110 @@ class AlumnoController extends BaseController
 			return Redirect::to('alumnos')->with('mensaje',$respuesta['mensaje']);
 		}
 	}
-	public function edit($id=null)
+	public function edit($cod)
 	{
-	
-			$alumno = Alumno::where('codAlumno','=',$id)->firstOrFail();
-			return View::make('alumno.edit',array('alumno'=>$alumno));
+		$alumno = Alumno::where('id','=',$cod)->firstOrFail();
+		return View::make('alumno.edit',array('alumno'=>$alumno));
 	}
-	public function update($id=0)
+	
+	public function deshabilitar($cod)
 	{
-		if($id<1)
+		DB::table('alumno')
+        		->where('id', $cod)
+        		->update(array('estado' => 0));
+		return Redirect::to('alumnos');
+	}
+
+	public function habilitar($cod)
+	{
+		DB::table('alumno')
+        		->where('id', $cod)
+        		->update(array('estado' => 1));
+		return Redirect::to('alumnos');
+	}
+
+	public function update($cod)
+	{
+		$alumno = Alumno::where('id','=',$cod)->firstOrFail();
+		if(is_object($alumno))
 		{
-			Redirect::to('404.html');
+			$alumno->nombre = Input::get('nombre');
+			$alumno->apellidos = Input::get('apellidos');
+			$alumno->dni = Input::get('dni');
+			$alumno->direccion = Input::get('direccion');
+			$alumno->telefono = Input::get('telefono');
+			$alumno->email = Input::get('email');
+			$alumno->save();
+			return Redirect::to('alumnos');
 		} else {
-			$alumno = Alumno::where('codAlumno','=',$id)->firstOrFail();
-			if(is_object($alumno))
-			{
-				$alumno->nombre = Input::get('nombre');
-				$alumno->apellidos = Input::get('apellidos');
-				$alumno->email = Input::get('email');
-				$alumno->telefono = Input::get('telefono');
-				$alumno->save();
-				return Redirect::to('alumnos');
-			} else {
-				Redirect::to('500.html');
-			}
+			Redirect::to('500.html');
 		}
 	}
-	public function profile($id = null)
+
+	public function updatePass($cod){
+		$alumno = Alumno::where('id', '=',$cod)->firstOrFail();
+		$pass_old = Input::get('pAnterior');
+		$pass = Input::get('password');
+		if(is_object($alumno))
+		{
+			$alumno->password = $pass;
+			$alumno->save();
+			return Redirect::to('alumnos');
+		} else {
+			Redirect::to('500.html');
+		}
+	}
+
+	public function profile($cod)
 	{
-		$alumno = alumno::where('codAlumno','=',$id)->firstOrFail();
+		$alumno = alumno::where('id','=',$cod)->firstOrFail();
 		if (is_object($alumno))
 		{
 			return View::make('alumno.profile',array('alumno'=>$alumno));
 		} else {
 			return Redirect::to('404.html');
 		}
+	}
+
+	public function imagen($id)
+	{
+			$alumno = Alumno::where('id','=',$id)->firstOrFail();
+			return View::make('alumno.imagen',array('alumno'=>$alumno));
+	}
+
+	public function uploadImage($id)
+	{
+		$mensaje = "Ocurrio Error";
+		if(Input::file("foto")->isValid())
+		{
+			$file = Input::file("foto");
+			$fileName = Input::file('foto')->getClientOriginalName();
+			$alumno = Alumno::where('id','=',$id)->firstOrFail();
+			$alumno->foto = md5($id."-".$fileName).'.'.Input::file('foto')->getClientOriginalExtension();
+			if($alumno->save())
+			{
+				Input::file('foto')->move('assets/foto',$alumno->foto);
+				$mensaje = "Imagen actualizado";
+			}
+		}
+		return Redirect::to('alumno/profile/'.$id)->withErrors($mensaje);
+	}
+
+	public function changePass($id = null)
+	{
+        if (is_null($id) or ! is_numeric($id))
+		{
+			return Redirect::to('404.html');
+		} else {
+			$alumno = Alumno::where('id','=',$id)->firstOrFail();
+			if (is_object($alumno))
+			{
+				$alumno->pasword = Input::get('password');
+				return View::make('alumno.change_pass',array('alumno'=>$alumno));
+			} else {
+				return Redirect::to('404.html');
+			}
+		}
+
 	}
 }
