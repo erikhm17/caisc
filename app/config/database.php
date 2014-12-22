7aa8165277d2432992934fcc2dc2547431298d06
fryzito <fryzito@users.noreply.github.com>
2014-12-22 05:00:40 -0500

    Restableciendo password BD
    Restableciendo a null

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'isc_test8',
 			'username'  => 'root',
-			'password'  => 'ma130107low',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
